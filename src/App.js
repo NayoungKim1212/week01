7a9d5c9e229a03edb41d104661f236de0d1e1784
NayoungKim1212 <kkd04250@naver.com>
2024-04-22 14:58:18 +0900

    comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-//import './App.css'; // ./=> 상대 경로
-import "App.css";
+import logo from './logo.svg';
+import './App.css';
 
-// 이 파일이 Playground
-function App() {
+function App() { // App : component component => 대문자로 시작
+  // 자바스크립트를 쓸 수 있는 영역
+  // const x = 1;
+  // function testFunc() {
+  // }  return 문 위 함수가 시작되기 전
   return (
+    // JSX 문법(HTML X)
+    // Javascript 문법을 사용하고 싶을 땐 {}
     <div className="App">
-      <p>Hello React!</p>
+      <header className="App-header">
+       {/* ex) {testFunc} */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
